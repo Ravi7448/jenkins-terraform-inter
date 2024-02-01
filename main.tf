93d01c29af27bf146656d19e4d29e5172daf0726
Ravi7448 <146948118+Ravi7448@users.noreply.github.com>
2024-02-01 11:04:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
-resource "aws_instance" "my-instance" {
-  count         = 3
-  ami           = "ami-05fb0b8c1424f266b"
-  instance_type = "t2.micro"
-  key_name      = "terrajenkins"
-   tags = {
-    Name  = "Terraform-${count.index + 1}"
+resource "aws_s3_bucket" "example" {
+  bucket = "madhu-devops123-vcube-114-8am"
+
+  tags = {
+    Name        = "My bucket"
     Environment = "Dev"
   }
 }
